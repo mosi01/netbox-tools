d1e230c25c7d4767ec30583094ebd314fda0a3de
mosi01 <149797690+mosi01@users.noreply.github.com>
2025-07-22 09:09:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-tools',
+    version='0.2',
+    description='A NetBox plugin for helpful network tools.',
+    url='https://github.com/YOUR_USERNAME/netbox-tools',
+    author='YOUR NAME',
+    author_email='YOUR_EMAIL@example.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
 
