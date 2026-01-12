36bd182c98cc3040b43a3d7226691fc1011238ea
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-12 07:53:01 +0100

    Update tool categories and add new menu items

--- nbtools/navigation.py
+++ nbtools/navigation.py
@@ -19,16 +19,39 @@ menu = PluginMenu(
 
 
         (
-            'Tools',
+            'Infrastucture Tools',
+            (
+                PluginMenuItem(
+                    link='plugins:nbtools:serial_checker',
+                    link_text='Serial Number Checker',
+                    auth_required=True,
+                ),
+            ),
+        ),
+
+        (
+            'Networking Tools',
+            (
+                PluginMenuItem(
+                    link='plugins:nbtools:prefix_validator',
+                    link_text='Prefix Validator',
+                    auth_required=True,
+                ),
+
+                PluginMenuItem(
+                    link='plugins:nbtools:ip_prefix_checker',
+                    link_text='IP Prefix Checker',
+                    auth_required=True,
+                ),
+
+            ),
+        ),
+        (
+            'Documentation Tools',
             (
                 PluginMenuItem(
                     link='plugins:nbtools:documentation_reviewer',
                     link_text='Documentation Reviewer',
-                    auth_required=True,
-                ),
-                PluginMenuItem(
-                    link='plugins:nbtools:serial_checker',
-                    link_text='Serial Number Checker',
                     auth_required=True,
                 ),
             ),
