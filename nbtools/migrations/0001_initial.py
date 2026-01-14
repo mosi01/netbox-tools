cee79b43050634073ffc43800dd274725d5b0490
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-14 14:47:14 +0100

    Update 0001_initial.py

--- nbtools/migrations/0001_initial.py
+++ nbtools/migrations/0001_initial.py
@@ -1,15 +1,33 @@
-from django.db import migrations
+from django.db import migrations, models
+
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-
-        #Placeholder
-
-    ]
+    dependencies = []
 
     operations = [
-            #Placeholder
+        migrations.CreateModel(
+            name='SharePointConfig',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('site_url', models.URLField()),
+                ('username', models.CharField(max_length=255)),
+                ('password', models.CharField(max_length=255)),
+                ('folder_mappings', models.JSONField(default=dict)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='DocumentationBinding',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('category', models.CharField(max_length=255)),
+                ('server_name', models.CharField(max_length=255, db_index=True)),
+                ('application_name', models.CharField(max_length=255, null=True, blank=True)),
+                ('file_name', models.CharField(max_length=255)),
+                ('version', models.CharField(max_length=50)),
+                ('file_type', models.CharField(max_length=50)),
+                ('sharepoint_url', models.URLField()),
+            ],
+        ),
     ]
