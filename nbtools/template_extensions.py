55f43d7ed08a726cd83ae02e098f9afc5a7e4d09
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-15 12:30:35 +0100

    Update template_extensions.py

--- nbtools/template_extensions.py
+++ nbtools/template_extensions.py
@@ -7,7 +7,7 @@ class VirtualMachineDocsExtension(PluginTemplateExtension):
         from .models import DocumentationBinding  # Lazy import
         vm = self.context['object']
         docs = DocumentationBinding.objects.filter(server_name=vm.name, category="Server")
-        return self.render('nbtools/vm_docs_box.html', extra_context={'docs': docs})
+        return self.render('nbtools/nbtools/vm_docs_box.html', extra_context={'docs': docs})
 
 class ApplicationDocsExtension(PluginTemplateExtension):
     model = 'dcim.device'  # Or your custom Application model if you have one
@@ -16,6 +16,6 @@ class ApplicationDocsExtension(PluginTemplateExtension):
         from .models import DocumentationBinding  # Lazy import
         device = self.context['object']
         docs = DocumentationBinding.objects.filter(application_name=device.name, category="Application")
-        return self.render('nbtools/app_docs_box.html', extra_context={'docs': docs})
+        return self.render('nbtools/nbtools/app_docs_box.html', extra_context={'docs': docs})
 
 template_extensions = [VirtualMachineDocsExtension, ApplicationDocsExtension]
