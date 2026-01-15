47fb818b2a26a54f587952086b4eb7cf77ca81af
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-15 20:12:38 +0100

    Update ui.py

--- nbtools/ui.py
+++ nbtools/ui.py
@@ -1,14 +1,10 @@
-from netbox.plugins.ui import ObjectSidebarPanel
+from netbox.plugins.ui import ObjectTab
+from virtualization.models import VirtualMachine
 
-class VirtualMachineSidebarPanel(ObjectSidebarPanel):
-    # Target model
-    model = 'virtualization.virtualmachine'
-    # Template to render
-    template = 'nbtool/vm_sidebar_panel.html'
-    # Panel title
-    label = 'Custom Info'
-    # Optional: position in sidebar
-    weight = 100
+class VirtualMachineCustomTab(ObjectTab):
+    model = VirtualMachine
+    label = "My Tab"
+    permission = "virtualization.view_virtualmachine"
+    template = "nbtools/vm_tab.html"
 
-# NetBox will auto-discover this list
-ui_components = [VirtualMachineSidebarPanel]
+ui_components = [VirtualMachineCustomTab]
