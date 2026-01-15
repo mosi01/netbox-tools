34f8c56928761930a095686fa63dc1ec4d3ff334
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-15 16:05:18 +0100

    Update __init__.py

--- nbtools/__init__.py
+++ nbtools/__init__.py
@@ -5,7 +5,7 @@ class NetboxToolsConfig(PluginConfig):
     name = "nbtools"
     verbose_name = "NetBox Tools"
     description = "Collection of tools for NetBox"
-    version = "0.1.0"
+    version = "0.4.0"
     author = "Simon Möller Ahlquist"
     author_email = "simon.moller@lindab.com"
     license = "MIT"
