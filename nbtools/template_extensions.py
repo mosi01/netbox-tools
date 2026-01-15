201667a023ef63f9b186c6be4e33983c16d7d2d3
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-15 12:39:21 +0100

    Update template_extensions.py

--- nbtools/template_extensions.py
+++ nbtools/template_extensions.py
@@ -6,7 +6,7 @@ class VirtualMachineDocsExtension(PluginTemplateExtension):
     def right_page(self):
         from .models import DocumentationBinding  # Lazy import
         vm = self.context['object']
-        docs = DocumentationBinding.objects.filter(server_name=vm.name, category="Server")
+        docs = DocumentationBinding.objects.filter(server_name__iexact=vm.name, category="Server")
         return self.render('nbtools/Panels/vm_docs_box.html', extra_context={'docs': docs})
 
 class ApplicationDocsExtension(PluginTemplateExtension):
@@ -15,7 +15,7 @@ class ApplicationDocsExtension(PluginTemplateExtension):
     def right_page(self):
         from .models import DocumentationBinding  # Lazy import
         device = self.context['object']
-        docs = DocumentationBinding.objects.filter(application_name=device.name, category="Application")
+        docs = DocumentationBinding.objects.filter(application_name__iexact=device.name, category="Application")
         return self.render('nbtools/Panels/app_docs_box.html', extra_context={'docs': docs})
 
 template_extensions = [VirtualMachineDocsExtension, ApplicationDocsExtension]
