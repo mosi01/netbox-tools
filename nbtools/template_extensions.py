7bad917c29c60eaeb107dfa6f6e914f49eada4b1
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-15 12:46:17 +0100

    Update template_extensions.py

--- nbtools/template_extensions.py
+++ nbtools/template_extensions.py
@@ -3,10 +3,10 @@ from netbox.plugins import PluginTemplateExtension
 
 class VirtualMachineDocsTab(PluginTemplateExtension):
     model = 'virtualization.virtualmachine'
-    tab = 'Documentation'  # This creates a new tab on the VM detail page
+    tab = 'Documentation'  # Creates a new tab on the VM detail page
 
-    def render(self):
-        from .models import DocumentationBinding  # Lazy import to avoid AppRegistryNotReady
+    def render(self, request):
+        from .models import DocumentationBinding
         vm = self.context['object']
         docs = DocumentationBinding.objects.filter(server_name__iexact=vm.name, category="Server")
         return self.render('nbtools/Tabs/vm_docs_box.html', extra_context={'docs': docs})
@@ -16,12 +16,11 @@ class ApplicationDocsTab(PluginTemplateExtension):
     model = 'dcim.device'  # Or your custom Application model if you have one
     tab = 'Documentation'  # Creates a new tab on the Device/Application detail page
 
-    def render(self):
+    def render(self, request):
         from .models import DocumentationBinding
         device = self.context['object']
         docs = DocumentationBinding.objects.filter(application_name__iexact=device.name, category="Application")
         return self.render('nbtools/Tabs/app_docs_box.html', extra_context={'docs': docs})
 
 
-# Register both template extensions
 template_extensions = [VirtualMachineDocsTab, ApplicationDocsTab]
