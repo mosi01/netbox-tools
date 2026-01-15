c67a596e1257b85db13626d0402476fc7bfbe6d1
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-15 12:09:11 +0100

    Update template_extensions.py

--- nbtools/template_extensions.py
+++ nbtools/template_extensions.py
@@ -1,10 +1,10 @@
 from netbox.plugins import PluginTemplateExtension
-from .models import DocumentationBinding
 
 class VirtualMachineDocsExtension(PluginTemplateExtension):
     model = 'virtualization.virtualmachine'
 
     def right_page(self):
+        from .models import DocumentationBinding  # Lazy import
         vm = self.context['object']
         docs = DocumentationBinding.objects.filter(server_name=vm.name, category="Server")
         return self.render('nbtools/vm_docs_box.html', extra_context={'docs': docs})
@@ -13,6 +13,7 @@ class ApplicationDocsExtension(PluginTemplateExtension):
     model = 'dcim.device'  # Or your custom Application model if you have one
 
     def right_page(self):
+        from .models import DocumentationBinding  # Lazy import
         device = self.context['object']
         docs = DocumentationBinding.objects.filter(application_name=device.name, category="Application")
         return self.render('nbtools/app_docs_box.html', extra_context={'docs': docs})
