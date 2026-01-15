a36c3f76b6919318a42d659faefca7c6354aa537
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-15 12:06:34 +0100

    Update template_extensions.py

--- nbtools/template_extensions.py
+++ nbtools/template_extensions.py
@@ -1,19 +1,21 @@
-from netbox.plugins import PluginTemplateExtension
+
+from extras.plugins import PluginTemplateExtension
 from .models import DocumentationBinding
 
-class VMDocumentationExtension(PluginTemplateExtension):
+class VirtualMachineDocsExtension(PluginTemplateExtension):
     model = 'virtualization.virtualmachine'
 
     def right_page(self):
         vm = self.context['object']
-        docs = DocumentationBinding.objects.filter(server_name=vm.name)
+        docs = DocumentationBinding.objects.filter(server_name=vm.name, category="Server")
         return self.render('nbtools/vm_docs_box.html', extra_context={'docs': docs})
 
-class ApplicationDocumentationExtension(PluginTemplateExtension):
-    model = 'dcim.device'  # Assuming applications are represented as devices or custom model
+class ApplicationDocsExtension(PluginTemplateExtension):
+    model = 'dcim.device'  # Or your custom Application model if you have one
+
     def right_page(self):
-        app = self.context['object']
-        docs = DocumentationBinding.objects.filter(application_name=app.name)
+        device = self.context['object']
+        docs = DocumentationBinding.objects.filter(application_name=device.name, category="Application")
         return self.render('nbtools/app_docs_box.html', extra_context={'docs': docs})
 
-template_extensions = [VMDocumentationExtension, ApplicationDocumentationExtension]
+template_extensions = [VirtualMachineDocsExtension, ApplicationDocsExtension]
