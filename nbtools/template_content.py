7cdac1adc93114be2203eb68b41433353eaa540e
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-16 07:52:22 +0100

    Update template_content.py

--- nbtools/template_content.py
+++ nbtools/template_content.py
@@ -5,17 +5,15 @@ class VirtualMachinePanel(PluginTemplateExtension):
     model = 'virtualization.virtualmachine'
 
     def right_page(self):
-        vm = self.context['object']  # Current VM object
-        # Fetch all documents assigned to this VM
+        vm = self.context['object']
         documents = DocumentationBinding.objects.filter(server_name=vm.name).order_by('category', 'file_name')
 
         if documents.exists():
-            # Group documents by category
             grouped_docs = {}
             for doc in documents:
                 grouped_docs.setdefault(doc.category, []).append(doc)
 
             return self.render('nbtools/panels/vm_panel.html', extra_context={'grouped_docs': grouped_docs})
-        return ''  # Hide panel if no document assigned
+        return ''
 
 template_extensions = [VirtualMachinePanel]
