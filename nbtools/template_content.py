a68e792784950391847100de14c2bc04b8b4251c
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-01-16 07:13:12 +0100

    Update template_content.py

--- nbtools/template_content.py
+++ nbtools/template_content.py
@@ -1,9 +1,15 @@
 from netbox.plugins import PluginTemplateExtension
+from .models import DocumentationBinding
 
 class VirtualMachinePanel(PluginTemplateExtension):
     model = 'virtualization.virtualmachine'
 
     def right_page(self):
-        return self.render('nbtools/vm_tab.html')
+        vm = self.context['object']  # Current VM object
+        # Find document assigned to this VM by server_name
+        document = DocumentationBinding.objects.filter(server_name=vm.name).first()
+        if document:
+            return self.render('nbtools/panel/vm_panel.html', extra_context={'document': document})
+        return ''  # Hide panel if no document assigned
 
 template_extensions = [VirtualMachinePanel]
