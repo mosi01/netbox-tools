4dede42d95b222139f0a68a7b2bf438a74b1879e
mosi01 <149797690+mosi01@users.noreply.github.com>
2026-07-07 09:55:23 +0200

    Refactor FortiSwitch port configuration views

--- nbtools/view_scripts/fortiswitch_port_configuration.py
+++ nbtools/view_scripts/fortiswitch_port_configuration.py
@@ -1,125 +1,19 @@
-from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views import View
+from netbox.views import generic
+from django.urls import reverse
 
-from nbtools.forms import FortiSwitchPortConfigurationForm
 from nbtools.models import FortiSwitchPortConfiguration
+from nbtools.forms import FortiSwitchPortConfigurationForm
 
 
-class FortiSwitchPortConfigurationListView(
-    LoginRequiredMixin,
-    PermissionRequiredMixin,
-    View,
-):
-    """
-    List predefined FortiSwitch port configurations.
-    """
-
-    permission_required = "nbtools.view_fortiswitchportconfiguration"
-    template_name = "nbtools/fortiswitch_port_configuration_list.html"
-
-    def get(self, request):
-        configurations = (
-            FortiSwitchPortConfiguration.objects
-            .select_related("site")
-            .order_by("site__name", "name")
-        )
-
-        return render(
-            request,
-            self.template_name,
-            {
-                "configurations": configurations,
-                "can_add": request.user.has_perm("nbtools.add_fortiswitchportconfiguration"),
-                "can_change": request.user.has_perm("nbtools.change_fortiswitchportconfiguration"),
-                "can_delete": request.user.has_perm("nbtools.delete_fortiswitchportconfiguration"),
-            },
-        )
+class FortiSwitchPortConfigurationListView(generic.ObjectListView):
+    queryset = FortiSwitchPortConfiguration.objects.select_related("site")
+    table = None  # optional if using table class later
 
 
-class FortiSwitchPortConfigurationEditView(
-    LoginRequiredMixin,
-    PermissionRequiredMixin,
-    View,
-):
-    template_name = "nbtools/fortiswitch_port_configuration_edit.html"
-
-    def has_permission(self):
-        pk = self.kwargs.get("pk")
-
-        if pk:
-            return self.request.user.has_perm(
-                "nbtools.change_fortiswitchportconfiguration"
-            )
-
-        return self.request.user.has_perm(
-            "nbtools.add_fortiswitchportconfiguration"
-        )
-
-    def get_object(self, pk):
-        if pk:
-            return get_object_or_404(FortiSwitchPortConfiguration, pk=pk)
-        return FortiSwitchPortConfiguration()
-
-    def get(self, request, pk=None):
-        obj = self.get_object(pk)
-        form = FortiSwitchPortConfigurationForm(instance=obj)
-
-        return render(
-            request,
-            self.template_name,
-            {
-                "object": obj,
-                "form": form,
-            },
-        )
-
-    def post(self, request, pk=None):
-        obj = self.get_object(pk)
-        form = FortiSwitchPortConfigurationForm(request.POST, instance=obj)
-
-        if form.is_valid():
-            form.save()
-            messages.success(request, "FortiSwitch port configuration saved.")
-            return redirect("plugins:nbtools:fortiswitch_port_configuration_list")
-
-        return render(
-            request,
-            self.template_name,
-            {
-                "object": obj,
-                "form": form,
-            },
-        )
+class FortiSwitchPortConfigurationEditView(generic.ObjectEditView):
+    queryset = FortiSwitchPortConfiguration.objects.all()
+    form = FortiSwitchPortConfigurationForm
 
 
-class FortiSwitchPortConfigurationDeleteView(
-    LoginRequiredMixin,
-    PermissionRequiredMixin,
-    View,
-):
-    """
-    Delete a predefined FortiSwitch port configuration.
-    """
-
-    permission_required = "nbtools.delete_fortiswitchportconfiguration"
-    template_name = "nbtools/fortiswitch_port_configuration_delete.html"
-
-    def get(self, request, pk):
-        obj = get_object_or_404(FortiSwitchPortConfiguration, pk=pk)
-
-        return render(
-            request,
-            self.template_name,
-            {
-                "object": obj,
-            },
-        )
-
-    def post(self, request, pk):
-        obj = get_object_or_404(FortiSwitchPortConfiguration, pk=pk)
-        obj.delete()
-
-        messages.success(request, "FortiSwitch port configuration deleted.")
-        return redirect("plugins:nbtools:fortiswitch_port_configuration_list")
+class FortiSwitchPortConfigurationDeleteView(generic.ObjectDeleteView):
+    queryset = FortiSwitchPortConfiguration.objects.all()
